33abf4bcba76f5a4e1ab7d7aa379e4afa4811d17
juzhishang <jyn@xkeshi.com>
2017-11-07 11:15:58 +0800

    feat: add room

--- index.js
+++ index.js
@@ -19,9 +19,13 @@ function handler(req, res) {
 io.sockets.on('connection', function(socket) {
     socket.on('clientMessage', function(content) {
         // 自己
-        socket.emit('serverMessage', 'You said: ' + content)
+        var room = socket.room || ''
+        if (room) {
+            socket.broadcast.to(room)
+        }
         // 发给其他用户(这个方法会排除发射方自身)
         socket.broadcast.emit('serverMessage', socket.username + ' said: ' + content)
+        socket.emit('serverMessage', 'You said: ' + content)
     })
 
     // 客户端发射login事件，服务端获取到用户名，然后广播用户登录
@@ -33,11 +37,22 @@ io.sockets.on('connection', function(socket) {
     })
     // 给客户端发射login事件，这样客户端就会提示用户输入登录名
     socket.emit('login')
-
     socket.on('disconnecting', function() {
         // 获取用户名
         var username = socket.username || socket.id
         socket.broadcast.emit('serverMessage', 'User ' + username + ' disconnected')
+    })
+
+    socket.on('join', function(room) {
+        var oldRoom = socket.room
+        socket.room = room
+        socket.join(room)
+        if (oldRoom) {
+            socket.leave(oldRoom)
+        }
+        var username = socket.username || socket.id
+        socket.emit('serverMessage', 'you joined room :' + room)
+        socket.broadcast.to(room).emit('serverMessage', 'User ' + username + ' joined this room')
     })
 })
 
